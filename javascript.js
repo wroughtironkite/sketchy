6ed1b6b095f44d122b8cccad79af08d65d4831f6
wroughtironkite <175876258+wroughtironkite@users.noreply.github.com>
2024-08-07 22:28:18 -0400

    Mess with margins some more

--- javascript.js
+++ javascript.js
@@ -4,7 +4,26 @@ container.style.display = "flex";
 function createGrid(size) {
 
 for (let rowcount = 0; rowcount < size; rowcount++) {
+    let boxHeight = "";
+    let rawWidth = 100;
+    if (size < 42) {
+        boxHeight = "50px";
+        rawWidth = 95;    
+    }
+    if (size >= 42 && size < 66) {
+        boxHeight = "25px";
+        rawWidth = 90;
+    }
+    if (size >= 66) {
+        boxHeight = "10px";
+        rawWidth = 85;
+    }
+    let rawBoxWidth = rawWidth/size;
+    let boxWidth = rawBoxWidth.toFixed(2) + "%";
     
+    
+
+
     const row = document.createElement("div");
     row.style.display = "flex";
     row.classList.add("row");
@@ -12,8 +31,8 @@ for (let rowcount = 0; rowcount < size; rowcount++) {
     for (let colcount = 0; colcount < size; colcount++) {
         const column = document.createElement("div");
         column.classList.add("box");
-        column.style.minWidth = "6%";
-        column.style.minHeight = "50px";
+        column.style.minWidth = boxWidth;
+        column.style.minHeight = boxHeight;
         row.appendChild(column);
     }
 
@@ -21,4 +40,6 @@ for (let rowcount = 0; rowcount < size; rowcount++) {
 }
 }
 
-createGrid(16);
+createGrid(25);
+
+
