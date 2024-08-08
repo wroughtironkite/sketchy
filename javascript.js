11ce3b90bce4399acfba50dc0d4765c913323b18
wroughtironkite <175876258+wroughtironkite@users.noreply.github.com>
2024-08-07 21:59:21 -0400

    Mess with margins

--- javascript.js
+++ javascript.js
@@ -1,16 +1,24 @@
 const container = document.querySelector("#container");
 container.style.display = "flex";
-for (let rowcount = 0; rowcount < 16; rowcount++) {
+
+function createGrid(size) {
+
+for (let rowcount = 0; rowcount < size; rowcount++) {
+    
     const row = document.createElement("div");
     row.style.display = "flex";
-    row.classList.add("box", "row");
+    row.classList.add("row");
 
-    for (let colcount = 0; colcount < 16; colcount++) {
+    for (let colcount = 0; colcount < size; colcount++) {
         const column = document.createElement("div");
         column.classList.add("box");
+        column.style.minWidth = "6%";
+        column.style.minHeight = "50px";
         row.appendChild(column);
     }
 
     container.appendChild(row);
 }
+}
 
+createGrid(16);
